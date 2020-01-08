1df777f71251a4206052a2f098badff28098a844
Efrencav <efren.cavazos@gmail.com>
2020-01-07 19:03:48 -0600

    revised contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,21 +6,7 @@ import Contact from "../components/Contact"
 export default function contact() {
   return (
     <Layout>
-      <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-        <input type="hidden" name="bot-field" />
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Name
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Message
-        <input type="text" name="message" />
-      </label>
-  </form>
+      <Contact />
     </Layout>
   )
 }
